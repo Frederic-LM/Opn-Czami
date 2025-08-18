c465c7567c81d118a0f3f47bc4ac0338467782d6
Frederic-LM <59538680+Frederic-LM@users.noreply.github.com>
2025-08-18 17:51:37 +0200

    Update utils.py

--- models/utils.py
+++ models/utils.py
@@ -1,1 +1,23 @@
+import logging
+from tkinter import messagebox
+from pathlib import Path
 
+# --- Application-Specific Enums & Data Structures ---
+from enum import Enum
+
+class KeyStorage(Enum):
+    """Enumeration for where a private key is stored."""
+    KEYSTORE = "STORED_IN_KEYSTORE"
+    FILE = "STORED_IN_FILE"
+
+# --- Helper Functions ---
+def show_error(title: str, message: str, log_error: bool = True):
+    """Convenience function for showing a messagebox error and logging it."""
+    if log_error:
+        logging.error(f"{title}: {message}")
+    messagebox.showerror(title, message)
+
+def show_info(title: str, message: str):
+    """Convenience function for showing a messagebox info dialog."""
+    logging.info(f"{title}: {message}")
+    messagebox.showinfo(title, message)
